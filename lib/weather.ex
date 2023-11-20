e035a5817094587e4753bfeaa0fc9933287019a9
zyadelhady <zyade40@gmail.com>
2023-11-20 23:54:13 +0200

    added basic functionaly to get the weather data

--- lib/weather.ex
+++ lib/weather.ex
@@ -6,17 +6,36 @@ defmodule Weather do
 
   def get(country) do
     country
-    |> generate_url
+    |> generate_url()
     |> HTTPoison.get!()
-    |> parse_json
+    |> parse_json()
+    |> get_weather_data()
+    |> Enum.map(&print_data/1)
   end
 
   def generate_url(country) do
-    "#{@weather_url_api}key=#{System.get_env("API_KEY")}&q=#{country}&days=3&aqi=no&alerts=yes"
+    "#{@weather_url_api}key=#{System.get_env("API_KEY")}&q=#{country}&days=2&aqi=no&alerts=yes"
   end
 
   def parse_json(%HTTPoison.Response{status_code: 200, body: body}) do
     Jason.decode!(body)
-    |> IO.inspect()
+  end
+
+  def parse_json(%HTTPoison.Response{status_code: 400, body: body}) do
+    IO.puts("No weather data is available for a country , please double check")
+    System.halt(2)
+  end
+
+  def parse_json({:error, %HTTPoison.Error{reason: reason}}) do
+    IO.inspect(reason)
+    System.halt(2)
+  end
+
+  def get_weather_data(%{"forecast" => %{"forecastday" => forecast_days}}) do
+    forecast_days
+  end
+
+  def print_data(%{"date" => date, "day" => day}) do
+    %{:date => date, :day => day}
   end
 end
