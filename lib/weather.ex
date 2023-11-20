5437a9920d86b9278bb32ec64a999ce26819ce8d
zyadelhady <zyade40@gmail.com>
2023-11-21 01:10:58 +0200

    working project for now

--- lib/weather.ex
+++ lib/weather.ex
@@ -2,15 +2,28 @@ defmodule Weather do
   @moduledoc """
   Documentation for `Weather`.
   """
+  alias Task
   @weather_url_api "https://api.weatherapi.com/v1/forecast.json?"
 
-  def get(country) do
+  def get(countries) do
+    countries
+    |> Enum.map(&spawn_get/1)
+    |> Enum.map(&Task.await/1)
+    |> List.flatten()
+    |> Enum.map(&print_table/1)
+  end
+
+  def spawn_get(country) do
+    Task.async(fn -> get_country(country) end)
+  end
+
+  def get_country(country) do
     country
     |> generate_url()
     |> HTTPoison.get!()
     |> parse_json()
-    |> get_weather_data()
-    |> Enum.map(&print_data/1)
+    |> get_forecast()
+    |> Enum.map(&get_day_data/1)
   end
 
   def generate_url(country) do
@@ -31,11 +44,44 @@ defmodule Weather do
     System.halt(2)
   end
 
-  def get_weather_data(%{"forecast" => %{"forecastday" => forecast_days}}) do
+  def get_forecast(%{"forecast" => %{"forecastday" => forecast_days}}) do
     forecast_days
   end
 
-  def print_data(%{"date" => date, "day" => day}) do
+  def get_day_data(%{"date" => date, "day" => day}) do
     %{:date => date, :day => day}
+  end
+
+  def print_table(%{
+        date: date,
+        day: %{
+          "avgtemp_c" => avg_temp_c,
+          "avghumidity" => avg_humidity,
+          "condition" => %{"text" => text},
+          "maxtemp_c" => max_temp,
+          "mintemp_c" => min_temp
+        }
+      }) do
+    IO.puts("""
+       ———————— #{date} ——————
+      |                          |
+      |                          |
+      |                          |
+      |                          |
+      |                          |
+      |                          |
+      |                          |
+       ——————————————————————————
+    """)
+
+    # IO.puts(" ———————————————————————")
+    # IO.puts(" |                     |")
+    # IO.puts(" |                     |")
+    # IO.puts(" |                     |")
+    # IO.puts(" |                     |")
+    # IO.puts(" |                     |")
+    # IO.puts(" |                     |")
+    # IO.puts(" |                     |")
+    # IO.puts(" ———————————————————————")
   end
 end
